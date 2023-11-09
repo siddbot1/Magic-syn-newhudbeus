16a64422aa835bfcca5a4dd719ddc56bab83b4c9
siddbot1 <146437902+siddbot1@users.noreply.github.com>
2023-11-09 12:58:30 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
 api_id = "25218337"
 api_hash = "0984be1572879b9dabb10882fb46a539"
-bot_token = "6376840344:AAFq2zhXDJijdgs88AT7pckugT4FbGzWjEw"
+bot_token = "6145493267:AAEcZpbcTQagy17lsSxG0NXWHXQdQoagk2g"
 sudo_groups = -1002061976269
